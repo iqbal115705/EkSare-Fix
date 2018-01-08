3fcd23281cc912543002a78fb7ccd0db4f998ed3
Iqbal115705 <sugandhiiqbal@gmail.com>
2018-01-08 23:33:24 +0700

    unit testing user

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe EkSare, type: :model do
+  it 'is databse authenticable' do
+    user = User.create(
+      email: 'test@example.com',
+      password: 'password123',
+      password_confirmation: 'password123')
+    expect(user.valid_password?('password123')).to be_truthy
+  end
+end
